7d527ebb51a5b826148059760534cdfeb59e5da8
EmiliaPlochocka <paradoksalnie.emu@gmail.com>
2025-10-21 21:57:58 +0200

    Update audits.ps1

--- audits.ps1
+++ audits.ps1
@@ -15,10 +15,20 @@ Get-ChildItem -Path "network_configs" -Recurse -File | Where-Object { $_.LastWri
 | Sort-Object LastWriteTime -Descending | Select-Object Name, LastWriteTime 
 | Format-Table -AutoSize
 
+#___GROUP FILE EXTENSIONS___
+Write-Host "`nFile extensions:"
 Get-ChildItem -Path "network_configs" -Recurse -file | Group-Object Extension 
-| Select-Object Name, count
+| Select-Object Name, count, @{Name = "TotalSize_MB"; Expression = { [math]::Round(($_.Group | Measure-Object Length -Sum).Sum / 1MB, 2) } } |
+Format-Table -AutoSize
 
-Write-Host "`nFive largest files:"
+#___LIST 5 LARGEST FILES___
+Write-Host "`nFive largest files: "
 Get-ChildItem -Path "network_configs" -Recurse -Filter "*.log" | Sort-Object Length -Descending 
 | Select-Object -first 5 Name, @{Name = "Size_MB"; Expression = { [math]::Round($_.Length / 1MB, 2) } }, LastWriteTime
 | Format-Table -AutoSize
+
+$ipPattern = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+$ipAddresses = Get-ChildItem -Path "network_configs" -Recurse -Filter "*.conf" |
+Select-String -Pattern $ipPattern |
+ForEach-Object { $_.Matches.Value } |
+Sort-Object -Unique
